de7e29ff647d6859db45a0f142374e7c2727fbc2
Jay <janjayjil@gmail.com>
2025-06-22 22:09:55 +0800

    feat (envSetter.util.php) : add PostgreSQL and MongoDB configuration loading

--- utils/envSetter.util.php
+++ utils/envSetter.util.php
@@ -4,6 +4,20 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
 $dotenv->load();
 
+$pgConfig = [
+    'host' => $_ENV['PG_HOST'],
+    'port' => $_ENV['PG_PORT'],
+    'db'   => $_ENV['PG_DB'],
+    'user' => $_ENV['PG_USER'],
+    'pass' => $_ENV['PG_PASS'],
+];
+
+$mongoConfig = [
+    'uri' => $_ENV['MONGO_URI'],
+    'db'  => $_ENV['MONGO_DB'],
+];
+
+// Optional: environment type logic
 $typeConfig = [
     'key' => $_ENV['ENV_NAME'] ?? 'development',
 ];
