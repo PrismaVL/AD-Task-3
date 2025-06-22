7bd724fa975eff60f221d5e94b864899f0e6b6c3
PrismaVL <janjayjil@gmail.com>
2025-06-22 18:35:13 +0800

    feat (envSetter.util.php) : added array key

--- utils/envSetter.util.php
+++ utils/envSetter.util.php
@@ -3,3 +3,7 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
 $dotenv->load();
+
+$typeConfig = [
+    'key' => $_ENV['ENV_NAME'],
+];
