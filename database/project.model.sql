23aa532e495d79f18bdf2ed37c2b3fa7aef4ef91
Jay <janjayjil@gmail.com>
2025-07-06 21:00:57 +0800

    feat (project.model.sql): change id field from SERIAL to UUID with default generation

--- database/project.model.sql
+++ database/project.model.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS projects (
-    id SERIAL PRIMARY KEY,
+    id uuid NOT NULL PRIMARY KEY DEFAULT gen_random_uuid(),
     name VARCHAR(100) NOT NULL,
     description TEXT,
     start_date DATE,
